b70c213c312dfc37b0ac3a734c530369bf513f27
shumcode <cshumylo@gmail.com>
2017-12-28 20:29:15 -0500

    adding files

---
+++ script.js
@@ -0,0 +1,11 @@
+$('body').smoothScroll({
+    delegateSelector: 'div.navbar a'
+  });
+
+//   $('p.subnav a').click(function(event) {
+//     event.preventDefault();
+//     var link = this;
+//     $.smoothScroll({
+//       scrollTarget: link.hash
+//     });
+//   });
