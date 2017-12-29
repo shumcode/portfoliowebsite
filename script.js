037807e80e3b982448ff12dada318e92bf002cca
shumcode <cshumylo@gmail.com>
2017-12-28 21:57:11 -0500

    i can't believe i fixed the scrolling

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-$('body').smoothScroll({
-    delegateSelector: 'div.navbar a'
+var mq = window.matchMedia( "(min-width: 768px)" );
+  $('div.navbar a').on("click", function() {
+    var mq = window.matchMedia( "(min-width: 768px)" );
+    if (mq.matches === true) {
+      $('body').smoothScroll({
+      offset: -75,
+      delegateSelector: 'div.navbar a'
+    
   });
+  } else {
+    $('body').smoothScroll({
+      offset: -175,
+      delegateSelector: 'div.navbar a'
+    
+  });
+  }
+  });
+
 
 //   $('p.subnav a').click(function(event) {
 //     event.preventDefault();
